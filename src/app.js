584ae34c4c967ae033ee0cccd437fd9550a00968
Anish-tech123 <rvermaji321@alumni.iastate.edu>
2024-08-22 23:07:52 +0000

    adding new features

--- src/app.js
+++ src/app.js
@@ -6,6 +6,14 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let card = document.querySelector(".card");
+  let rank = document.querySelector(".rank");
+  let suits = ["heart", "spade", "club", "diamond"];
+  let ranks = ["A", 2, 3, 4, 5, 6, 7, 8, 9, 10, "J", "Q", "K"];
+  const getRandomInt = array => {
+    return Math.floor(Math.random() * array.length);
+  };
+
+  card.classList.add(suits[getRandomInt(suits)]);
+  rank.innerHTML = ranks[getRandomInt(ranks)];
 };
